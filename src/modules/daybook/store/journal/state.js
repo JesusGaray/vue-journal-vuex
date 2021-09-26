30688c0de4c157a41ff9d9b25521a26c52327c19
Jesus Garay <jesus_fco7@hotmail.com>
2021-09-26 14:58:38 -0500

    Prmer commit gaD

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default ()=>({
+    isLoading:true,
+    entries:[
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text:'lorem asdasd rg ef gdf gd fg fdg fdg as dasd  er yrt ty u sd g dfg dfg df gdf g v sdf as das das dasgsdf g dfg d a sfd asd.',
+            picture:null,
+        },
+        {
+            id: new Date().getTime()+1000,
+            date: new Date().toDateString(),
+            text:'kjnkjnkjn jknkj ybaisd onasnknc oasoin oasckanslkcn aiscnakls cn ias lfkasnd sinfsodf ihsoi sdv dyuoiscosic siojsdo iocjdsoc iojsdocjsd.',
+            picture:null,
+        },
+        {
+            id: new Date().getTime()+2000,
+            date: new Date().toDateString(),
+            text:'sdfsdklkl lkansklcn aoisc oasoi anscapasojcpansco nuash ciuasbnauisn anoisnaoisjoicjaoishcoiahso ihoa isho iohaosoiasoi hoiahsioha  iah.',
+            picture:null,
+        },
+    ]
+})
